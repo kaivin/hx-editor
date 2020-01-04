13acedccc5cce29168cbf0299c2f6e22835f5962
kevin <iqkevin@aliyun.com>
2020-01-04 11:15:43 +0800

    修改按钮

--- src/components/editorData/buttonData/modules/nyb01.js
+++ src/components/editorData/buttonData/modules/nyb01.js
@@ -8,7 +8,7 @@ export function getHtmlData (){
         align:'center', // left(左对齐)|center(居中对齐)|right(右对齐)        
         style:'other', // icon(图标)|number(数字)|other(其他)
         fileName:'nyb01.js', // 该文件的文件名
-        styleWebCode:'.smallbtn001{clear:both;display:block;text-align:center;}.smallbtn001 a{clear:both;display:inline-block;width:400px;}', // PC端样式
+        styleWebCode:'.smallbtn001{clear:both;display:block;text-align:center;}.smallbtn001 a{clear:both;display:inline-block;width:400px;}.smallbtn001 a img{max-width:100%;width:100%;}', // PC端样式
         styleWapCode:'.smallbtn001{width:9.2rem;margin:0.4rem auto;}.smallbtn001 a{clear:both;display:block;}', // 移动端样式
         htmlString:'<p class="smallbtn001"><a href="#"><img src="'+image+'" alt=""></a></p>'
     };
