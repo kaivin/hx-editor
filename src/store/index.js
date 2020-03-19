e950acc1c2c7bcbf24d1da2cdffd8ac3aaf81219
kevin <iqkevin@aliyun.com>
2020-03-19 08:15:24 +0800

    修改图文

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex';
 import getters from './getters';
 
 Vue.use(Vuex);
-
 const modulesFiles = require.context('./modules', false, /\.js$/);
 // you do not need `import app from './modules/app'`
 // it will auto require all vuex module from modules file
