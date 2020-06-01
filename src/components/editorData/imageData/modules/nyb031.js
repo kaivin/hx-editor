3c01c7d87d4b92635fb03656cc59578b1ba84fa5
smilelife <njh66666666@126.com>
2020-06-01 15:00:03 +0800

    样式优化

--- src/components/editorData/imageData/modules/nyb031.js
+++ src/components/editorData/imageData/modules/nyb031.js
@@ -4,8 +4,8 @@ export function getHtmlData (){
         color:'红色', // 多个颜色用逗号分隔 目前取值项：红色,橙色,蓝色,绿色,棕色,黄色,紫色,粉色,黑色,青色,灰色
         effect:'static', // dynamic(动态)|static(静态)
         fileName:'nyb031.js', // 该文件的文件名
-        styleWebCode:'.smalltext031{border: 1px solid #bf483b;padding-top: 38px;margin: 24px 0 24px 17px;} .smalltext031 p{padding: 14px 3.4% 28px;margin: 0!important;} .smalltext031 .tit{background: #bf483b;border-radius: 6px;font-size: 18px!important;color: #fff!important;padding: 0 20px;display: inline-block;position: relative;left: -17px;} .smalltext031 .tit strong{color: #fff;}', // PC端样式
-        styleWapCode:'.smalltext031{border: 1px solid #bf483b;padding-top: 0.72rem;margin: 0.4rem 0 0.4rem 0.26667rem;} .smalltext031 p{padding: 0.33333rem 4.4% 0.6rem;margin: 0!important;} .smalltext031 .tit{background: #bf483b;border-radius: 0.08rem;font-size: 0.42667rem!important;color: #fff!important;padding: 0 0.32rem;display: inline-block;position: relative;left: -0.26667rem;} .smalltext031 .tit strong{color: #fff;}', // 移动端样式
+        styleWebCode:'.smalltext031{border: 1px solid #bf483b;padding-top: 38px;margin: 24px 0 24px 17px;} .smalltext031 p{padding: 0 3.4%;margin: 14px 0!important;} .smalltext031 .tit{background: #bf483b;border-radius: 6px;font-size: 18px!important;color: #fff!important;padding: 0 20px;margin: 10px 0!important;display: inline-block;position: relative;left: -17px;} .smalltext031 .tit strong{color: #fff;}', // PC端样式
+        styleWapCode:'.smalltext031{border: 1px solid #bf483b;padding-top: 0.72rem;margin: 0.4rem 0 0.4rem 0.26667rem;} .smalltext031 p{padding: 0 4.4%;margin: 0.24rem 0!important;} .smalltext031 .tit{background: #bf483b;border-radius: 0.08rem;font-size: 0.42667rem!important;color: #fff!important;padding: 0 0.32rem;margin: 0.2rem 0!important;display: inline-block;position: relative;left: -0.26667rem;} .smalltext031 .tit strong{color: #fff;} ', // 移动端样式
         htmlString:'<div class="smalltext031"> <p class="tit"><strong>标题标题标题</strong></p> <p>正文正文正文正文正文</p> <p class="tit"><strong>标题标题标题</strong></p> <p>正文正文正文正文正文</p> <p class="tit"><strong>标题标题标题</strong></p> <p>正文正文正文正文正文</p> </div>'
     };
 }
